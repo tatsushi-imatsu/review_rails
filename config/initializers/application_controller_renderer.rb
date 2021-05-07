75f7da979f0066f459c54a2e5e52585c291c26da
tatsushi-imatsu <tatsu0320itwl@gmail.com>
2021-05-07 00:49:13 +0000

    profile registration

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'c9f1bd74ed69c1ba051980d505450d74febe81b81c4177598d780d2359e72c1150dbd7ee6ca40c394cbbb15e2234b279b6cbd2cc63622bacea08d1a20fa51e4d'
